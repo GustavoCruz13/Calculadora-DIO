071e15b00f7253e6a614a3aba7ad88666a04b911
GustavoCruz13 <ghcruz13@gmail.com>
2023-11-20 17:51:58 -0300

    feat: adding a feature and refactor
    Coloquei em "execução" a última feature que criei
    que eu esqueci e tratei uns erros.

--- bin/calculadora_dart_dio.dart
+++ bin/calculadora_dart_dio.dart
@@ -3,20 +3,35 @@ import 'dart:io';
 
 void main(List<String> args) {
   print("Calculadora em Dart Iniciando...");
-  double number1 = double.parse(lerConsole("Informe o primeiro número:"));
-  double number2 = double.parse(lerConsole("Informe o segundo número:"));
+  double number1 = lerConsoleParsed("Informe o primeiro número:");
+  double number2 = lerConsoleParsed("Informe o segundo número:");
   String operador = lerConsole("Informe o operador: (+, -, * e /)");
   calcularBasico(operador, number1, number2);
 }
 
 double lerConsoleParsed(String texto) {
-  return double.parse(lerConsole(texto));
+  var valor = double.tryParse(lerConsole(texto));
+  if (valor == null) {
+    print("O número informado está incorreto, o resultado da operação será 0.");
+    exit(0);
+  } else {
+    return valor;
+  }
 }
 
 String lerConsole(String texto) {
   print(texto);
   var line = stdin.readLineSync(encoding: utf8);
   return line ?? " ";
+}
+
+double divisao(double number1, double number2) {
+  if (number2 == 0) {
+    print("Operação Invalida, não é possível dividir por 0");
+    exit(0);
+  } else {
+    return number1 / number2;
+  }
 }
 
 void calcularBasico(String operador, double number1, double number2) {
@@ -32,12 +47,12 @@ void calcularBasico(String operador, double number1, double number2) {
       resultado = number1 * number2;
       break;
     case "/":
-      resultado = number1 / number2;
+      resultado = divisao(number1, number2);
       break;
     default:
       {
         print("Operação Invalida");
-        exit(1);
+        exit(0);
       }
   }
   print("O resultado da operação $operador foi: $resultado");
